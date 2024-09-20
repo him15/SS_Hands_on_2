b4611b60943e48f24b12698c3720c62908b21a48
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-09-20 11:46:47 +0530

    SIGINT commited

---
+++ 8b.c
@@ -0,0 +1,43 @@
+/*
+Name:- HIMANSHU SHIVHARE
+Roll No:- MT2024058
+Program:-8. Write a separate program using signal system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+d. SIGALRM (use alarm system call)
+e. SIGALRM (use setitimer system call)
+f. SIGVTALRM (use setitimer system call)
+g. SIGPROF (use setitimer system call)
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+void print()
+{
+    printf("SIGINT caught\n");
+    _exit(0);
+}
+
+void main()
+{
+    __sighandler_t status;
+
+    status = signal(SIGINT, (void *)print);
+    if (status == SIG_ERR)
+    {
+        perror("Error while assigning signal handler!");
+    }
+    else
+    {
+        raise(SIGINT); // SIGINT signal is sent to a process by its controlling terminal when a user wishes to interrupt the process
+    }
+}
+
+/*
+	OUTPUT- 
+
+SIGINT caught
+
+*/
