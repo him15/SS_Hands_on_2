f8861b140c09f19fc8f584bb0b2682d023446b91
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-09-20 11:44:45 +0530

    SIGSEGV commited

---
+++ 8a.c
@@ -0,0 +1,44 @@
+/*
+Name:- HIMANSHU SHIVHARE
+Roll No:- MT2024058
+Program:-8. Write a separate program using signal system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+d. SIGALRM (use alarm system call)
+e. SIGALRM (use setitimer system call)
+f. SIGVTALRM (use setitimer system call)
+g. SIGPROF (use setitimer system call)
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+void print()
+{
+    printf("SIGSEGV caught\n");
+    _exit(0);
+}
+
+void main()
+{
+    __sighandler_t status;
+
+    status = signal(SIGSEGV, (void *)print);
+    if (status == SIG_ERR)
+    {
+        perror("Error while assigning signal handler!");
+    }
+    else
+    {
+        raise(SIGSEGV); //// Send segmentation fault signal
+    }
+}
+
+/*
+OUTPUT- 
+
+himanshu@himanshu-Inspiron-15-3567:~/Desktop/Hanson2$ ./a.out
+SIGSEGV caught
+
+*/
