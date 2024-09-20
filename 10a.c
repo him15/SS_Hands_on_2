991769c374b5f958d8a6868eb583e16e8b685138
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-09-20 14:12:36 +0530

    SIGSEGV

---
+++ 10a.c
@@ -0,0 +1,74 @@
+/*
+Name:- HIMANSHU SHIVHARE
+Roll No:- MT2024058
+program:- 10. Write a separate program using sigaction system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+void print(){
+    printf("Caught SIGSEGV\n");
+}
+void main()
+{
+    int status;
+    struct sigaction act;
+    
+    act.sa_handler=print;//function that replace default action of signal SIGSEGV
+    act.sa_flags=0;
+    status = sigaction(SIGSEGV,&act,NULL);
+    if (status == -1)
+    {
+        perror("Error while assigning signal handler!");
+    }
+    else
+    {
+        raise(SIGSEGV);
+    }
+}
+
+/*
+
+/*
+Name:- Padaliya Shivam
+Roll No:- MT2024107
+program:- 10. Write a separate program using sigaction system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+void print(){
+    printf("Caught SIGSEGV\n");
+}
+void main()
+{
+    int status;
+    struct sigaction act;
+    
+    act.sa_handler=print;//function that replace default action of signal SIGSEGV
+    act.sa_flags=0;
+    status = sigaction(SIGSEGV,&act,NULL);
+    if (status == -1)
+    {
+        perror("Error while assigning signal handler!");
+    }
+    else
+    {
+        raise(SIGSEGV);
+    }
+}
+
+/*
+OUTPUT-
+
+Caught SIGSEGV
+
+*/
