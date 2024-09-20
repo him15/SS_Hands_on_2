cbab97f36d4bd42f3affff7b34ffb837c76055aa
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-09-20 11:53:02 +0530

    Alarm system call

---
+++ 8d.c
@@ -0,0 +1,43 @@
+/*
+Name:- HIMANSHU SHIVHARE
+Roll No:- MT2024058
+Program:-8. Write a separate program using signal system call to catch the following signals.
+a. SIGSEGV
+b. SIGINT
+c. SIGFPE
+d. SIGALRM (use alarm system call)
+e. SIGALRM (use setitimer system call)
+f. SIGVTALRM (use setitimer system call)
+g. SIGPROF (use setitimer system call)
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+void print()
+{
+    printf("SIGALRM caught\n");
+    _exit(0);
+}
+
+void main()
+{
+    __sighandler_t status;
+    alarm(2); // Set an alarm for 2 second
+    status = signal(SIGALRM, (void *)print);
+    if (status == SIG_ERR)
+    {
+        perror("Error while assigning signal handler!");
+    }
+    else
+    {
+        while (1)
+            ; // waiting for alarm
+    }
+}
+
+/*
+OUTPUT-
+
+SIGALRM caught
+*/
