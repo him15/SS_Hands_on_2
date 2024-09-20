92148a33fb2a8b881d135e48b43f12276281b774
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-09-21 01:35:01 +0530

    Program 20b

---
+++ 20b.c
@@ -0,0 +1,42 @@
+/*
+Name:- HIMANSHU SHIVHARE
+Roll No:- MT2024058
+Program:-19. Create a FIFO file by
+a. mknod command
+b. mkfifo command
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+
+int main() {
+    int stat = mkfifo("myFifo", 0777);
+    if(stat == -1) {
+        perror("FIFO creation using mkfifo failed");
+        exit(1);
+    } else {
+        printf("FIFO Success using mkfifo!!!\n");
+    }
+
+    // Create a FIFO using mknod with permissions
+    stat = mknod("mknodFifo", S_IFIFO | 0666, 0);
+    if(stat == -1) {
+        perror("FIFO creation using mknod failed");
+        exit(1);
+    } else {
+        printf("mknod FIFO Success!!!\n");
+    }
+
+    return 0;
+}
+
+/*
+
+OUTPUT-
+
+FIFO Success using mkfifo!!!
+mknod FIFO Success!!!
+*/
