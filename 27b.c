2ad4690f30c01f2f9bb5678cea8c6fdff69d61bd
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-09-21 20:45:38 +0530

    Program 27b

---
+++ 27b.c
@@ -0,0 +1,52 @@
+/*
+Name:- HIMANSHU SHIVHARE
+Roll No:- MT2024058
+Program:-27. Write a program to receive messages from the message queue.
+a. with 0 as a flag
+b. with IPC_NOWAIT as a flag
+*/
+
+
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+typedef struct mesg_buffer
+{
+    /* data */
+    long mesg_type;
+    char mesg_text[100];
+} mesg_buffer;
+int main()
+{
+    /* code */
+    mesg_buffer message;
+    // generate unique no
+    key_t key = ftok(".", 1); // projext path is current directory and project id=1
+    // create massage queue using msgget
+    //  msgget creates a message queue
+    //  and returns identifier
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+    printf("enter msgtype:\n");
+    scanf("%ld", &message.mesg_type);
+    int size_s = msgrcv(msgid, &message, sizeof(message), message.mesg_type, IPC_NOWAIT); // IPC_NOWAIT will not wait for msg and give us warning if message of type is not in queue
+    if (size_s <= 0)
+    {
+        perror("No message in queue\n");
+    }
+    else
+    {
+        printf("Message: %s\n", message.mesg_text);
+    }
+    return 0;
+}
+
+/*
+OUTPUT- 
+
+
+enter msgtype:
+hyyyy
+No message in queue
+: No message of desired type
+*/
